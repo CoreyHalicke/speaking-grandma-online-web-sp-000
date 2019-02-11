859a9327f2bf0754b30ce4ddbb4ac8a18068c0b9
Corey H <c.l.halicke@gmail.com>
2019-02-11 15:29:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif 
-    string == string.downcase
+    string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif 
     string == string.upcase
